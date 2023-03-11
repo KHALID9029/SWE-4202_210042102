6ef302788c6111911d86cef19e3f621b8a7b0200
KHALID HASAN ADOR <khalidhasan@iut-dhaka.edu>
2023-03-11 18:06:16 +0600

    Added Lab-01 Task

---
+++ LAB-01/Lab Task/Lab Task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 99;
+            double myDouble = 70.50;
+            bool myBool = true;
+            Console.WriteLine(Convert.ToString(myInt));
+            Console.WriteLine(Convert.ToDouble(myInt));
+            Console.WriteLine(Convert.ToInt32 (myDouble));
+            Console.WriteLine(Convert.ToString(myBool));
+            Console.ReadKey();
+
+        }
+        
+    }
+}
