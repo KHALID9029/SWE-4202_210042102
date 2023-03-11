7edb12cf117f3a095ac46139d34081ec82ea7171
KHALID HASAN ADOR <khalidhasan@iut-dhaka.edu>
2023-03-11 18:57:14 +0600

    Added Lab-06 Task

---
+++ Lab-06/Lab-06/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_06
+{
+    internal class Book
+    {
+        private int Id;
+        private string Title;
+        private string Author;
+        private string Publisher;
+        private int Quantity;
+
+        public Book(int id, string title, string author, string publisher, int quantity)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            Publisher = publisher;
+            Quantity = quantity;
+        }
+
+        public int GetId() { return Id; }
+        public string GetTitle() { return Title; }
+        public string GetAuthor() { return Author; }
+
+        public string GetPublisher() { return Publisher;}
+        public int GetQuantity() { return Quantity;}
+        public Book() { }
+
+        public void SetId(int id) { Id= id; }
+        public void SetTitle(string title) { Title= title; }
+        public void SetAuthor(string author) { Author= author; }
+        public void SetPublisher(string publisher) { Publisher= publisher; }
+        public void SetQuantity(int quantity) { Quantity= quantity; }
+    }
+}
---
+++ Lab-06/Lab-06/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_06
+{
+   
+    abstract internal class User
+    {
+        public int Id;
+        public string Name;
+        public string Address;
+        public string Type;
+    }
+
+    class RegularUser : User 
+    {
+        
+
+        public RegularUser(int id, string name, string address)
+        {
+            Id= id; Name= name; Address= address; Type = "Regular";
+        }
+    }
+
+    class SpecialUser : User
+    {
+        List<Book> books = new List<Book>();
+        public SpecialUser(int id, string name, string address)
+        {
+            Id = id; Name = name; Address = address; Type = "Special";
+        }
+
+        public SpecialUser() { }
+
+        public void AddBook(Book b)
+        {
+            books.Add(b);
+        }
+
+        public void DeleteBook(Book b) 
+        {
+            foreach (Book bb in books)
+            {
+                if (bb.GetId() == b.GetId())
+                { b = bb; break; }
+            }
+            books.Remove(b);
+        }
+    }
+}
